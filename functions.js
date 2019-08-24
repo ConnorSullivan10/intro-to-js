4d53db90491ad98fb34712f4b133f36bf771a12a
Connor Sullivan <cksullvn@gmail.com>
2019-08-24 11:24:17 -0500

    FUNCTIONS: went over functions and nuggets'

---
+++ functions.js
@@ -0,0 +1,90 @@
+console.log('functions')
+
+const fancyFunction = (something) => {
+    return something + 'fancy';
+}
+
+let output = fancyFunction('michael is ');
+console.log(output);
+
+output = 'something else';
+
+let firstName = 'George';
+let lastName = 'Weasley';
+
+console.log(`${firstName} ${lastName}`)
+
+firstName = 'Bill';
+lastName = 'Weasley';
+
+console.log (`${firstName} ${lastName}`)
+
+firstName = 'Ginny';
+lastName = 'Weasley';
+
+console.log (`${firstName} ${lastName}`)
+
+//es6
+const sayMyName = (first, last) => {
+    // do something
+    return `${first} ${last}`;
+    //console.log(first + ' ' +last)
+}
+
+//es5
+// const sayMyName = function (first, last) {
+// // do something
+//   console.log(`${first} ${last}`)
+// }
+
+console.log(sayMyName('Callan', 'Morrison'))
+
+
+// NUGGET TIME
+
+const nuggetizer = (animal) => {
+    return `nugget ${animal}`;
+}
+
+const humaneNuggetizer = (animal) => {
+    return animal;
+}
+
+const spamFactory = (literallyAnything) => {
+    return 'spam (lunch)';
+}
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('octopus'));
+console.log(nuggetizer('john wark'));
+
+console.log(humaneNuggetizer('chicken'));
+console.log(humaneNuggetizer('octopus'));
+console.log(humaneNuggetizer('john wark'));
+
+console.log(spamFactory('chicken'));
+console.log(spamFactory('octopus'));
+console.log(spamFactory('john wark'));
+
+const printToDOM = (toPrint) => {
+    document.getElementById('output').innerHTML += toPrint;//something
+}
+
+let bandNumber = 1
+
+const takeNumber = (bandName) => {
+    /*
+    Write your awesome code here. See comments
+    below for what should be returned.
+*/
+
+printToDOM(`<h3>${bandNumber}) ${bandName}</h3>`);
+bandNumber++
+};
+
+takeNumber("Galactic Scum") // This should display "1. Galactic Scum" in the DOM
+takeNumber("Underdogs") //This should display "2. Underdogs" in the DOM
+
+
+
+// printToDOM('Connor');
